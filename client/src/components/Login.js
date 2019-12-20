452d9c6e009e9a9d1c63de6c82a2a2eca1df619f
yutveg <kodi.0n31l@gmail.com>
2019-12-20 08:22:49 -0800

    project initialized some components built for private routing

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const postRequest = e => {
+    e.preventDefault();
+  };
+
+  const handleChange = e => {
+    setCredentials({
+      [e.target.name]: e.target.value
+    });
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={postRequest}>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <label>Password:</label>
+        <input
+          type="text"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
     </>
   );
 };
